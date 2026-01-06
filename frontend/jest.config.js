5f8efccc98df6b45f0865b5a8a641500aff8de87
Adolfo <alopezoy@gmail.com>
2026-01-06 15:34:29 +0100

    test: configure HTML reporter for unit tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,6 +4,18 @@ const config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Tibia Huntrack test report",
+        includeFailureMsg: true,
+        includeConsoleLog: true,
+        sort: "titleAsc",
+      },
+    ],
+  ],
 };
 
 export default config;
